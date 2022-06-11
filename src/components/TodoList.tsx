81e37848cd7e6e832062fdbdfd411e08270bddad
IlyaAgarishev <ilyagarishev@gmail.com>
2022-06-11 23:19:11 +0500

    drew the todo data

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,8 +6,12 @@ const TodoList: React.FC = () => {
   const { error, limit, loading, page, todos } = useTypedSelector(
     (state) => state.todo
   );
-
   const { fetchTodos, setTodoPage } = UserActions();
+  const pages = [1, 2, 3, 4, 5];
+
+  useEffect(() => {
+    fetchTodos(page, limit);
+  }, [page, limit]);
 
   if (loading) {
     return <h1>Идет загрузка ....</h1>;
@@ -22,10 +26,30 @@ const TodoList: React.FC = () => {
       {todos.map((todo) => {
         return (
           <div key={todo.id}>
-            {todo.id} - {todo.name}
+            {todo.id} - {todo.title}
           </div>
         );
       })}
+
+      <div style={{ marginTop: 20 }}>
+        {pages.map((p) => {
+          return (
+            <span
+              key={p}
+              style={{
+                padding: 5,
+                border: p === page ? "2px solid green" : "1px solid black",
+                cursor: "pointer",
+              }}
+              onClick={() => {
+                setTodoPage(p);
+              }}
+            >
+              {p}
+            </span>
+          );
+        })}
+      </div>
     </div>
   );
 };
